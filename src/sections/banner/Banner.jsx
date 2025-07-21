2f1eb4234b5879fb4b4653f797e96fc5da5289cd
Maëlle Nioche <maelle.nioche@gmail.com>
2025-07-21 17:48:42 +0200

    Update: add a text to say that this is my old portfolio

--- src/sections/banner/Banner.jsx
+++ src/sections/banner/Banner.jsx
@@ -1,17 +1,25 @@
 import datas from '../../assets/datas.json';
 
 function Banner() {
-  return (
-    <section id="banner">
-      <div className="image">
-        <div className="img-maelle"></div>
-      </div>
+  return (      
+      <section id="banner">
+        <div className='welcome-message card'>
+          <p>Bienvenue cher visiteur !</p>
+          <p>Je vous informe que vous vous trouvez actuellement sur mon ancien portfolio (2024).</p>
+          <p>Pour me contacter ou découvrir mon activité actuelle, rendez-vous sur le site de mon entreprise : {' '}
+          <a href="https://koji-dev.fr/">koji-dev.fr</a></p>
+        </div>
+        <div className="banner-content">
+        <div className="image">
+          <div className="img-maelle"></div>
+        </div>
 
-      <div>
-        <h1>Maëlle Nioche</h1>
-        <strong>{datas.job}</strong>
-      </div>
-    </section>
+        <div>
+          <h1>Maëlle Nioche</h1>
+          <strong>{datas.job}</strong>
+        </div>
+        </div>
+      </section>
   );
 }
 
