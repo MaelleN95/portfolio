7303d2586a62fd8e9f88e50e64453f01164f749e
Maëlle Nioche <maelle.nioche@gmail.com>
2025-07-22 10:24:29 +0200

    Update : move the social media links section to the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,22 @@ function Footer() {
     return (
         <footer>
             <span>
-                Made with passion by Maëlle Nioche © {new Date().getFullYear()}
+                Made with passion by Maëlle Nioche © 2024
             </span>
+            <div>
+                <a href="https://www.linkedin.com/in/maelle-nioche" title="Lien vers Linkedin">
+                    <img        
+                        src="/linkedin-icon.svg"
+                        alt="Logo de Linkedin"
+                    />
+                </a>
+                <a href="https://github.com/MaelleN95/" title="Lien vers Github">
+                    <img        
+                        src="/github-icon-2.svg"
+                        alt="Logo de Github"
+                    />
+                </a>
+            </div>
         </footer>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Formation from '../sections/formation/Formation';
 import GoodPractices from '../sections/good-practices/GoodPractices';
 import HardSkills from '../sections/hard-skills/HardSkills';
 import MyProjects from '../sections/my-projects/MyProjects';
-import SocialMedia from '../sections/social-media/SocialMedia';
 import SoftSkills from '../sections/soft-skills/SoftSkills';
 import Tools from '../sections/tools/Tools';
 
@@ -21,7 +20,6 @@ function Home() {
       <Formation />
       <MyProjects />
       <Contact />
-      <SocialMedia />
     </main>
   );
 }
